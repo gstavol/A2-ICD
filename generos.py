1de9d832a8c787f61d367588e55a0f8072e6755d
Brunikito <brunoluiszrosa@gmail.com>
2024-06-24 20:38:35 -0300

    Tabela com os gêneros dos filmes

---
+++ generos.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+listinha = []
+dataset = pd.read_csv('./Filmes.csv')
+datand = dataset.values
+#print(datand[0, 1:])
+
+headers = ['Filme', 'Animação', 'Família', 'Aventura', 'Comédia', 'Ficção científica', 'Ação', 'Drama', 'Crime', 'Thriller', 'Fantasia', 'Guerra', 'Terror', 'Mistério', 'Romance', 'História', 'Música']
+dataset = pd.DataFrame(columns=headers)
+
+rows = []
+# body
+
+for filme in datand:
+    row = []
+    nome = filme[1]
+    row.append(nome)
+    for genero in headers[1:]:
+        if genero in eval(filme[5]):
+            row.append(1)
+        else:
+            row.append(0)
+    rows.append(row)
+
+for each_row in rows:
+    dataset.loc[len(dataset)] = each_row
+
+dataset.to_csv("Generos.csv")
