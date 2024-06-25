c3d2e756059b67ce3b74a5e712ceb53304d540c6
Brunikito <brunoluiszrosa@gmail.com>
2024-06-24 22:09:47 -0300

    Formatação das resenhas com o chatgpt

---
+++ gepeto.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+
+def resumo_gpt_e_nota(lista_recebida):
+    client = OpenAI(api_key='')
+
+    prompt = [{"role":"user", "content":'''
+        Resuma a seguinte resenha, como se fosse você quem escreveua resenha. Evite termos como "a resenha", "uma crítica":
+        {lista_recebida[1]}
+        Dê também uma nota que reflita esta resenha para o filme.
+        Lembre-se que a nota deve ser condizente com a resenha.
+        A sua resposta deve ser formatada da seguinte forma:
+        ['NOTA', 'RESUMO']
+        A nota deve estar no formato XX%.
+        Lembre-se de adicionar os apóstrofos e colchetes.
+        A sua resposta não deve conter nada além disso.
+            '''
+            }]
+
+    response = client.chat.completions.create(
+        messages = prompt,
+        model = 'gpt-3.5-turbo-0125',
+        max_tokens = 1000,
+        temperature = 1
+    )
+
+    return eval(response.choices[0].message.content)
+
+print(resumo_gpt_e_nota(lista_teste))
