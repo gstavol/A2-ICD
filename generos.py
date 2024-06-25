77f4a73899f6eca6967b9410062acbbf9db04b76
Brunikito <brunoluiszrosa@gmail.com>
2024-06-24 22:11:36 -0300

    Formatação das resenhas com o chatgpt

--- generos.py
+++ generos.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 listinha = []
 dataset = pd.read_csv('./Filmes.csv')
-datand = dataset.values
-#print(datand[0, 1:])
+nddata = dataset.values
 
 headers = ['Filme', 'Animação', 'Família', 'Aventura', 'Comédia', 'Ficção científica', 'Ação', 'Drama', 'Crime', 'Thriller', 'Fantasia', 'Guerra', 'Terror', 'Mistério', 'Romance', 'História', 'Música']
 dataset = pd.DataFrame(columns=headers)
@@ -11,7 +10,7 @@ dataset = pd.DataFrame(columns=headers)
 rows = []
 # body
 
-for filme in datand:
+for filme in nddata:
     row = []
     nome = filme[1]
     row.append(nome)
